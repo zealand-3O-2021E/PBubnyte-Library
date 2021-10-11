8af4a855a568b950796a794c652e1ed49d6523c3
Paulina Bubnytė <bubneja@gmail.com>
2021-10-11 15:29:40 +0200

    Assignment 1

--- Library/Book.cs
+++ Library/Book.cs
@@ -4,10 +4,10 @@ namespace Library
 {
     public class Book
     {
-        int ISBN13 { get; set; }
-        string Title { get; set; }
-        string Author { get; set; }
-        int PageNumber { get; set; }
+        public string ISBN13 { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PageNumber { get; set; }
 
     }
 }
