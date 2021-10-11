bd64d776cd28fc48b2878fba722660b2c4129f3d
Paulina Bubnytė <bubneja@gmail.com>
2021-10-11 13:26:21 +0200

    Add project files.

---
+++ Library/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Library
+{
+    public class Book
+    {
+        int ISBN13 { get; set; }
+        string Title { get; set; }
+        string Author { get; set; }
+        int PageNumber { get; set; }
+
+    }
+}
